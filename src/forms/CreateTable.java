8a7c766d4e7270632df86b84f28311cf379212c3
K <khoadh28@gmail.com>
2020-07-02 08:40:17 +0700

    add search table

--- src/forms/CreateTable.java
+++ src/forms/CreateTable.java
@@ -4,29 +4,35 @@ import utils.DBConnection;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 public class CreateTable extends JScrollPane {
     // table = ten bang
     // columnName = ten cac cot
     // query = sql query
-    public JScrollPane table(String[] columnName, String query, Dimension d) {
+    public static JLabel[] labels,labelshead;
+    public static JPanel[] pnlData ;
+    public static JButton[] btnEdit;
+    public static int row,column;
+    public static JPanel pnlAllData;
+    public JScrollPane table(String table, String[] columnName, String query, Dimension d) {
         DBConnection db = new DBConnection();
         // lay so hang + so cot bang muon ve
-        int row = db.getRowCount(query);
+        row = db.getRowCount(table);
         // lay so cot muon ve
-        int column = columnName.length;
+        column = columnName.length;
         // tao panel tong chua panelheader va paneldata
         JPanel panel = new JPanel();
         //set layout
         panel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         // tao array label chua data
-        JLabel[] labels = new JLabel[column * row + 1];
+        labels = new JLabel[column * row + 1];
         // lay du lieu bang
         ArrayList<Object> ob = db.getAllData(query);
         // tao array label chua ten cot
-        JLabel[] labelshead = new JLabel[column];
+        labelshead = new JLabel[column];
         // tao panel chua label header
         JPanel pnlheader = new JPanel();
         pnlheader.setLayout(new GridLayout(1, column));
@@ -35,37 +41,139 @@ public class CreateTable extends JScrollPane {
         for (JLabel item : labelshead
         ) {
             item = new JLabel();
-            item.setBorder(BorderFactory.createLineBorder(Color.black));
+            item.setBorder(BorderFactory.createMatteBorder(1, 0, 1, 1, Color.black));
+            item.setHorizontalAlignment(SwingConstants.CENTER);
+            item.setVerticalAlignment(SwingConstants.CENTER);
             item.setText(columnName[name]);
+            item.setPreferredSize(d);
             pnlheader.add(item);
             name++;
         }
+        //tao panel chua panel data
+         pnlAllData = new JPanel();
+        pnlAllData.setLayout(new GridLayout(row, 0));
         //tao panel chua label data
-        JPanel pnlData = new JPanel();
-        pnlData.setLayout(new GridLayout(row, column));
-        //tao array label chua data
-        JLabel[] labelsData = new JLabel[row * column];
+         pnlData = new JPanel[row];
         // ve bang label data
         int z = 1;
         for (int i = 0; i < row; i++) {
+            // khoi tao panel data
+            pnlData[i] = new JPanel();
+            pnlData[i].setLayout(new GridLayout(0,column));
             for (int y = 0; y < column; y++) {
+                // khoi tao label data
                 labels[z] = new JLabel();
+                labels[z].setVerticalAlignment(SwingConstants.CENTER);
+//                labels[z].setHorizontalAlignment(SwingConstants.CENTER);
                 labels[z].setText(ob.get(z - 1).toString());
-                labels[z].setBorder(BorderFactory.createLineBorder(Color.black));
-                pnlData.add(labels[z]);
+                labels[z].setBorder(BorderFactory.createMatteBorder(0, 0, 1, 1, Color.black));
+                labels[z].setPreferredSize(d);
+                pnlData[i].add(labels[z]);
                 z++;
             }
+            pnlAllData.add(pnlData[i]);
         }
-        //add panel heaeder va panel data vao panel tong
+        //add panel header va panel data vao panel tong
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridy = 0;
         c.gridx = 0;
-        pnlheader.setPreferredSize(d);
         panel.add(pnlheader, c);
         c.gridy = 1;
-        pnlData.setPreferredSize(d);
-        panel.add(pnlData, c);
+
+        panel.add(pnlAllData, c);
         JScrollPane sp = new JScrollPane(panel);
         return sp;
+    }
+
+    public JScrollPane table(String table, String[] columnName, String query, Dimension d, boolean editable) {
+
+        DBConnection db = new DBConnection();
+        // lay so hang + so cot bang muon ve
+        row = db.getRowCount(table);
+        // lay so cot muon ve
+        column = columnName.length;
+        // tao panel tong chua panelheader va paneldata
+        JPanel panel = new JPanel();
+        //set layout
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        // tao array label chua data
+        labels = new JLabel[column * row + row + 1];
+        // lay du lieu bang
+        ArrayList<Object> ob = db.getAllData(query);
+        // tao array label chua ten cot
+         labelshead = new JLabel[column];
+        // tao panel chua label header
+        JPanel pnlheader = new JPanel();
+        pnlheader.setLayout(new GridLayout(1, column));
+        // ve bang ten cot
+        int name = 0;
+        for (JLabel item : labelshead
+        ) {
+            item = new JLabel();
+            item.setBorder(BorderFactory.createMatteBorder(1, 0, 1, 1, Color.black));
+            item.setHorizontalAlignment(SwingConstants.CENTER);
+            item.setVerticalAlignment(SwingConstants.CENTER);
+            item.setText(columnName[name]);
+            pnlheader.add(item);
+            item.setPreferredSize(d);
+            name++;
+        }
+        // add o action vao cuoi label header
+        JLabel lblAction = new JLabel("Action");
+        lblAction.setBorder(BorderFactory.createMatteBorder(1, 0, 1, 1, Color.black));
+        lblAction.setHorizontalAlignment(SwingConstants.CENTER);
+        lblAction.setVerticalAlignment(SwingConstants.CENTER);
+        pnlheader.add(lblAction);
+        //tao panel chua panel data
+        pnlAllData = new JPanel();
+        pnlAllData.setLayout(new GridLayout(row,0));
+        //tao panel chua label data
+         pnlData = new JPanel[row];
+        //tao array label edit
+        btnEdit = new JButton[row];
+        // ve bang label data
+        int z = 1;
+        for (int i = 0; i < row; i++) {
+            //khoi tao panel data
+            pnlData[i] = new JPanel();
+            pnlData[i].setLayout(new GridLayout(0,column+1));
+            for (int y = 0; y < column; y++) {
+                labels[z] = new JLabel();
+                labels[z].setVerticalAlignment(SwingConstants.CENTER);
+//                labels[z].setHorizontalAlignment(SwingConstants.CENTER);
+                labels[z].setText(ob.get(z - 1).toString());
+                labels[z].setBorder(BorderFactory.createMatteBorder(0, 0, 1, 1, Color.black));
+                labels[z].setPreferredSize(d);
+                pnlData[i].add(labels[z]);
+                z++;
+            }
+            pnlAllData.add(pnlData[i]);
+            //add edit row
+            btnEdit[i] = new JButton();
+            btnEdit[i].setText("<HTML><FONT color=\"#006ce5\"><U>Edit</U></FONT>"
+                    + " </HTML>");
+            btnEdit[i].setHorizontalAlignment(SwingConstants.CENTER);
+            btnEdit[i].setBorder(BorderFactory.createMatteBorder(0, 0, 1, 1, Color.black));
+            btnEdit[i].setOpaque(false);
+            btnEdit[i].setBackground(Color.WHITE);
+            btnEdit[i].addActionListener(this::ActionEvent);
+            btnEdit[i].setPreferredSize(d);
+            pnlData[i].add(btnEdit[i]);
+        }
+        //add panel header va panel data vao panel tong
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridy = 0;
+        c.gridx = 0;
+        panel.add(pnlheader, c);
+        c.gridy = 1;
+        panel.add(pnlAllData, c);
+        JScrollPane sp = new JScrollPane(panel);
+        return sp;
+
+    }
+
+    public void ActionEvent(ActionEvent e) {
+
     }
 }
--- src/forms/DepotForm.java
+++ src/forms/DepotForm.java
@@ -1,5 +1,6 @@
 package forms;
 
+import utils.ChangePosition;
 import utils.setUIFont;
 
 import javax.swing.*;
@@ -7,27 +8,102 @@ import javax.swing.plaf.FontUIResource;
 import java.awt.*;
 
 public class DepotForm extends JDialog {
-    public DepotForm () {
+    public DepotForm() {
+        //setting form
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setBounds(100,100,1000,500);
+        setBounds(100, 100, 1000, 500);
         setModal(true);
         setResizable(false);
         setUIFont f = new setUIFont();
-        f.Font(new FontUIResource("Arial", Font.PLAIN,12));
+        f.Font(new FontUIResource("Arial", Font.PLAIN, 12));
         setTitle("Depot");
-        setLayout(new BorderLayout());
-        JPanel pnlData = new JPanel();
-        pnlData.setBorder(BorderFactory.createLineBorder(Color.black));
-        String [] columnname = {"ID","Quantity","Price"};
-        String query = "Select * from depot";
-        Dimension d =  new Dimension(200,50);
-        JScrollPane sp = new CreateTable().table(columnname,query,d);
-        pnlData.setLayout(new GridLayout(1,0));
-        pnlData.add(sp);
-        add(pnlData,BorderLayout.SOUTH);
-        JPanel pnlComponent = new JPanel();
-        pnlComponent.setBorder(BorderFactory.createLineBorder(Color.black));
-        add(pnlComponent,BorderLayout.NORTH);
+        //add root panel
+        JPanel rootPnl = (JPanel) getContentPane();
+        rootPnl.setLayout(new BoxLayout(rootPnl, BoxLayout.Y_AXIS));
+        //create box
+        Box[] boxes = new Box[2];
+        boxes[0] = Box.createHorizontalBox();
+        boxes[1] = Box.createHorizontalBox();
+
+        boxes[0].createGlue();
+        boxes[1].createGlue();
+
+        rootPnl.add(boxes[0]);
+        rootPnl.add(boxes[1]);
+
+        //add JPanel head
+        JPanel pnlHead = new JPanel();
+        pnlHead.setLayout(null);
+
+        //add component to panel head
+
+        JPanel pnlSearch = new JPanel();
+        pnlSearch.setToolTipText("Search");
+        pnlSearch.setLayout(null);
+        pnlSearch.setBounds(10, 10, 400, 130);
+        pnlHead.add(pnlSearch);
+
+        JTextField tfSearch = new JTextField();
+        tfSearch.setBounds(10, 40, 200, 25);
+        pnlSearch.add(tfSearch);
+
+        String[] boxColumn = {"All", "Name", "Content", "Quantity", "Price", "Supplier"};
+        JComboBox boxSearch = new JComboBox(boxColumn);
+        boxSearch.setBounds(230, 40, 120, 25);
+        pnlSearch.add(boxSearch);
+
+        JButton btnSearch = new JButton("Search");
+        btnSearch.setBounds(120, 80, 120, 25);
+        btnSearch.addActionListener(e -> {
+            int position = 0;
+            int index = boxSearch.getSelectedIndex();
+            String strSearch = tfSearch.getText();
+            int row = CreateTable.row;
+            int column = 0;
+            if (index == 0) {
+                for (JPanel item : CreateTable.pnlData
+                ) {
+                    item.show();
+                }
+            } else {
+
+                for (int i = 1; i <= row; i++) {
+                    if (!CreateTable.labels[index + column].getText().matches(".*" + strSearch + ".*")) {
+                        CreateTable.pnlData[i - 1].hide();
+                    } else {
+                        CreateTable.pnlData[i - 1].show();
+                        ChangePosition c = new ChangePosition();
+                        c.swap(CreateTable.pnlAllData, position, i - 1);
+                        position++;
+                    }
+
+                    column += CreateTable.column;
+                }
+                CreateTable.pnlAllData.validate();
+                CreateTable.pnlAllData.repaint();
+            }
+
+        });
+        pnlSearch.add(btnSearch);
+        //add JPanel body
+        JPanel pnlBody = new JPanel();
+        pnlBody.setLayout(new GridLayout(1, 0));
+        //add component to panel body
+        String[] columnname = {"Name", "Content", "Quantity", "Price", "Supplier"};
+        String query = "select productname , productcontent, depot.quantity, depot.price, suppliername\n" +
+                "from depot join product on  depot.productid = product.productid\n" +
+                "join supplier on product.supplierid = supplier.supplierid";
+        Dimension d = new Dimension(164, 20);
+        JScrollPane sp = new CreateTable().table("Depot", columnname, query, d, true);
+        pnlBody.add(sp);
+
+        //add 2 panel to box
+        pnlBody.setPreferredSize(new Dimension(1300, 400));
+        pnlHead.setPreferredSize(new Dimension(1300, 200));
+        boxes[0].add(pnlHead);
+        boxes[1].add(pnlBody);
+
+
         setVisible(true);
     }
 }
