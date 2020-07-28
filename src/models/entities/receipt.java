7563edbbdb0c666e719035c81e163334384beeb4
K <khoadh28@gmail.com>
2020-07-29 00:48:13 +0700

    receipt entity

--- src/models/entities/receipt.java
+++ src/models/entities/receipt.java
@@ -1,4 +1,18 @@
 package models.entities;
 
+import java.sql.Timestamp;
 public class receipt {
+    public String receipttype,productid;
+    public float price;
+    public int supplierid,customerid,employeeid;
+    public Timestamp date;
+
+    public receipt(String receipttype, float price, int supplierid, int employeeid, Timestamp date) {
+        this.receipttype = receipttype;
+        this.price = price;
+        this.supplierid = supplierid;
+        this.employeeid = employeeid;
+        this.date = date;
+        customerid = 0;
+    }
 }
